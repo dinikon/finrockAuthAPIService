2fa11af99bf3dc6442134ffa99e3d5b2d98fc7a6
inikon <denis.inikon@gmail.com>
2024-09-21 15:08:04 +0300

    Initial commit

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
 from starlette.middleware.cors import CORSMiddleware
 
 from core.config import settings
@@ -23,6 +24,7 @@ async def lifespan(app: FastAPI):
 
 
 main_app = FastAPI(
+    default_response_class=ORJSONResponse,
     lifespan=lifespan,
 )
 
